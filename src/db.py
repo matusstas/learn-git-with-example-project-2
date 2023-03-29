bf81d2ee434fd1b11ae0281a87bd73e3d5dcdd52
Matúš Staš <matusstas.ms@gmail.com>
2023-03-29 16:08:18 +0200

    Add docstrings

--- src/db.py
+++ src/db.py
@@ -3,4 +3,7 @@ from tinydb import TinyDB
 db = TinyDB("db.json")
 
 def insert_post(post):
+    """
+    Insert post into db
+    """
     db.insert(post)
--- src/modules/post.py
+++ src/modules/post.py
@@ -1,4 +1,7 @@
 import db
 
 def create_post(title, text):
+    """
+    Create post
+    """
     db.insert_post({title: "a", text: "b"})
